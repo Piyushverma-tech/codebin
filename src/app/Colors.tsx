21cf8ecc138737ae851bf525432cf3060a73d1b6
Piyushverma-tech <piyush626480@gmail.com>
2025-01-20 23:46:53 +0530

    added clerk auth and mongo db

---
+++ src/app/Colors.tsx
@@ -0,0 +1,1 @@
+export const mainColor = 'violet-600';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,91 @@
-import Image from "next/image";
+'use client';
+import { useAuth } from '@clerk/nextjs';
+import CodeIcon from '@mui/icons-material/Code';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="poppins">
+      <Navbar />
+      <CTASection />
+    </div>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+function Navbar() {
+  return (
+    <div className="flex m-5 max-sm:mt-9 mx-8 items-center  justify-between max-sm:flex-col">
+      <Logo />
+      <Buttons />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex gap-2 items-center">
+      <div className={`bg-violet-600 p-[6px] rounded-md`}>
+        <CodeIcon sx={{ fontSize: 27, color: 'white' }} />
+      </div>
+      <div className="flex gap-1 text-[19px]">
+        <span className={`font-bold text-violet-600`}>Codebin</span>
+      </div>
+    </div>
+  );
+}
+
+function Buttons() {
+  const { userId } = useAuth();
+  return (
+    <div className="max-sm:w-full">
+      {userId ? (
+        <Link href="/my-notes">
+          <button
+            className={`max-sm:w-full bg-violet-600 p-[8px] px-6 text-sm text-white rounded-md`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Access The App
+          </button>
+        </Link>
+      ) : (
+        <div className="flex gap-2 max-sm:flex-col max-sm:w-full max-sm:mt-8">
+          <Link href="/sign-in">
+            <button
+              className={`max-sm:w-full bg-violet-600 p-[8px] text-sm text-white rounded-md`}
+            >
+              Sign In
+            </button>
+          </Link>
+          <Link href="/sign-up">
+            <button
+              className={`max-sm:w-full text-sm border border-violet-600 text-violet-600 hover:bg-violet-600 hover:text-white p-[8px] px-6 rounded-md`}
+            >
+              Sign up
+            </button>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+    </div>
+  );
+}
+
+function CTASection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6">
+      <h2 className="font-bold text-3xl text-center">
+        Organize your code snippets
+        <span className={`text-violet-600`}>Efficiently</span>
+      </h2>
+      <p className="text-center text-md w-[450px] max-sm:w-full text-slate-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quae
+        repellat quaerat nam deleniti dignissimos tempore temporibus, eos,
+        beatae hic voluptatum explicabo quas assumenda pariatur.
+      </p>
+      <button
+        className={`block px-9 py-2 text-lg bg-violet-600 font-medium text-white transition focus:outline-none rounded-full`}
+        type="button"
+      >
+        {"let's get started!"}
+      </button>
     </div>
   );
 }
