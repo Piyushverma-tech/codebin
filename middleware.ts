2fd69ad77c078332eac1c3f9fe3e9a2882b7bd86
Piyushverma-tech <piyush626480@gmail.com>
2026-01-27 02:01:43 +0530

    strengthening routes protection

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
+  if (req.nextUrl.searchParams.has('__clerk_handshake')) {
+    return;
+  }
+
   if (!isPublicRoute(req)) {
-    (await auth()).redirectToSignIn();
+    auth.protect();
   }
 });
 
