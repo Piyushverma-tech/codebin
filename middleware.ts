3e5de086949295d7a5a5bdf4aeaadb03e48d5242
Piyushverma-tech <piyush626480@gmail.com>
2026-01-27 13:47:17 +0530

    deployment issue - testing 1

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, req) => {
-  if (req.nextUrl.searchParams.has('__clerk_handshake')) {
-    return;
-  }
-
-  if (!isPublicRoute(req)) {
-    auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/(api|trpc)(.*)'],
+  matcher: ['/((?!_next|.*\\..*).*)'],
 };
