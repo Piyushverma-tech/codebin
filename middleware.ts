5531d63f9e6082d27fc81011840e20e3293e3aaf
Piyushverma-tech <piyush626480@gmail.com>
2026-01-27 14:18:27 +0530

    Fixing immidiate handsake issue

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (req.nextUrl.pathname.startsWith('/my-snippets')) {
+    if (!userId) {
+      return NextResponse.redirect(new URL('/sign-in', req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ['/((?!_next|.*\\..*).*)'],
